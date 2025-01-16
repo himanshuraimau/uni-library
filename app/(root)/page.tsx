bb238ae0700ef8f3fedbac078e8c0b289886099f
Himanshu Rai <himanshuraimau9@gmail.com>
2025-01-16 18:32:09 +0530

    Add sample book data and update BookOverview and home layout components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
 import Image from "next/image";
 
 const home = () =><>
-        <BookOverview/>
-        <BookList/>
+        <BookOverview {...sampleBooks[0]}/>
+        <BookList
+         title="Latest Books",
+         books={sampleBooks},
+         containerClassName="mt-28"
+        />
 
 </>
 
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-const BookOverview = () => {
-  return (
-    <section className='book-overview'>
-         <div className='flex flex-1 flex-col gap-5'>
-                 <h1>A Long Book Title</h1>
-         </div>
-    </section>
-  )
+const BookOverview = ({ title,
+    author,
+    genre,
+    rating,
+    total_copies,
+    available_copies,
+   description,
+    color,
+    cover ,
+}:Book) => {
+    return (
+        <section className='book-overview'>
+            <div className='flex flex-1 flex-col gap-5'>
+                <h1>A Long Book Title</h1>
+            </div>
+        </section>
+    )
 }
 
 export default BookOverview;
